5fce7393037a7541e0a435a2c28f601cba22cea4
aaditya-1219 <aadityapatil1910@gmail.com>
2023-10-30 10:13:16 +0530

    Fully working

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+const MongoClient = require('mongodb').MongoClient
+const url = "mongodb+srv://aaditya-1219:mKYPr7BkweCQYtWv@cluster0.tllkell.mongodb.net/"
+
+app.use(express.static('static'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'index.html')));
+
+
+app.get('/', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/dept', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'dept.html'));
+});
+
+app.get('/contact', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.get('/dept/civil', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'civil.html'));
+});
+app.get('/dept/mech', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'mech.html'));
+});
+app.get('/dept/cse', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'cse.html'));
+});
+app.get('/dept/eln', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'eln.html'));
+});
+app.get('/dept/ele', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'ele.html'));
+});
+app.get('/dept/it', async(req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'it.html'));
+}); 
+
+app.get('/:deptName/data', async (req, res) => {
+    const deptName = req.params.deptName
+    MongoClient.connect(url).then((client) => {
+        const connect = client.db("wce-dept");
+        const collection = connect.collection("dept")
+        collection.findOne({dept: deptName}).then((data) => {
+            res.json(data)
+          });
+      })
+
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
